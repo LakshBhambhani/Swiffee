be34f7bf413de81d62e920eccee4b36c645396fc
Laksh Bhambhani <lakshbh@gmail.com>
2019-05-20 21:31:48 -0700

    Built a basic starting

---
+++ README.md
@@ -0,0 +1,6 @@
+# Team 670's Website
+Find us at [homesteadrobotics.com](http://homesteadrobotics.com/)!
+
+We also have updates in the form of a blog at [Team 670 Updates](https://team670updates.github.io).
+
+### Only changes made in the gh-pages branch will affect what's on the website.
---
+++ header.php
@@ -0,0 +1,49 @@
+<?php
+<header id="header">
+        <nav class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
+                </div>
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.html">Home</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li class="active"><a href="about-us.html">About Us</a></li>
+                                <li><a href="history.html">History</a></li>
+                                <li><a href="pastrobot.html">Past Competition Robots</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="about-first.html">About FIRST</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Meet the Team<i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="meet-us-leadership.html">The Leadership Team</a></li>
+                                <li><a href="meet-us-mentors.html">The Mentors</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="outreach.html">Outreach</a></li>
+                        <li><a href="sponsor.html">Sponsors</a></li>
+                        <li><a href="resources.html">Resources</a></li> 
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Media<i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="videos.html">Videos</a></li>
+                                <li><a href="gallery.html">Gallery</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact-us.html">Contact</a></li>      
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->
+    </header><!--/header-->
+?>
